b8272144351b0ba7ed55e0df245360ab96cf7c09
Riley Gibson <riley2.gibson2@gmail.com>
2022-10-12 16:25:36 +1300

    Adding even more style changes

--- src/main/webapp/js/reports.js
+++ src/main/webapp/js/reports.js
@@ -91,12 +91,74 @@ class ReportsPage extends Page {
 	}
 
 	generateReport() {
-		insertLoading(screen.width*0.75, screen.height*0.55, false);
+		//insertLoading(screen.width*0.75, screen.height*0.55, false);
+		
+		this.makeThumb(window);
+
 	}
 
 	animateEntrance(start) {
 		setTimeout(removeLoading, start);
 		setTimeout(fadeIn, start, $("#rpCont"));
+	}
+
+	makeThumb(page) {
+
+
+
+
+	   //
+  // If absolute URL from the remote server is provided, configure the CORS
+  // header on that server.
+  //
+  const url = '"https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf"';
+
+  //
+  // The workerSrc property shall be specified.
+  //
+  pdfjsLib.GlobalWorkerOptions.workerSrc =
+    '../../node_modules/pdfjs-dist/build/pdf.worker.js';
+
+  //
+  // Asynchronous download PDF
+  //
+  const loadingTask = pdfjsLib.getDocument(url);
+  (async () => {
+    const pdf = await loadingTask.promise;
+    //
+    // Fetch the first page
+    //
+    const page = await pdf.getPage(1);
+    const scale = 1.5;
+    const viewport = page.getViewport({ scale });
+    // Support HiDPI-screens.
+    const outputScale = window.devicePixelRatio || 1;
+
+    //
+    // Prepare canvas using PDF page dimensions
+    //
+    const canvas = document.getElementById("rpFile");
+    const context = canvas.getContext("2d");
+
+    canvas.width = Math.floor(viewport.width * outputScale);
+    canvas.height = Math.floor(viewport.height * outputScale);
+    canvas.style.width = Math.floor(viewport.width) + "px";
+    canvas.style.height = Math.floor(viewport.height) + "px";
+
+    const transform = outputScale !== 1 
+      ? [outputScale, 0, 0, outputScale, 0, 0] 
+      : null;
+
+    //
+    // Render PDF page into canvas context
+    //
+    const renderContext = {
+      canvasContext: context,
+      transform,
+      viewport,
+    };
+    page.render(renderContext);
+  })();
 	}
 
 }
