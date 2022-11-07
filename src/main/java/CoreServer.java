0549dd55b2b30128a7c0174707f325dfd0dc8980
Riley Gibson <riley2.gibson2@gmail.com>
2022-11-08 00:40:04 +1300

    Mucking around with web paths

--- src/main/java/CoreServer.java
+++ src/main/java/CoreServer.java
@@ -2,11 +2,14 @@ package main.java;
 
 import java.io.File;
 
+import org.eclipse.jetty.rewrite.handler.RedirectRegexRule;
+import org.eclipse.jetty.rewrite.handler.RewriteHandler;
+import org.eclipse.jetty.rewrite.handler.RewritePatternRule;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
-import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 import main.java.core.Utils;
 import main.java.debug.CLI;
@@ -17,7 +20,7 @@ import main.java.mock.DataMockup;
 import main.java.mqtt.MQTTManager;
 
 public class CoreServer {
-	
+
 	private static Server server;
 	public static SessionManager sessionManager;
 	public static MQTTManager mqttManager;
@@ -31,19 +34,36 @@ public class CoreServer {
 		CLI.initialise();
 		CLI.debug(Loc.CORE, "Webapp path: "+webappPath);
 
+		//Rewrites
+		RewriteHandler rewriteHandler = new RewriteHandler();
+		rewriteHandler.setRewriteRequestURI(true);
+		rewriteHandler.setRewritePathInfo(true);
+		rewriteHandler.setOriginalPathAttribute("requestedPath");
+		RewritePatternRule rule = new RewritePatternRule();
+		rule.setPattern("/webapp");
+		rule.setReplacement("/webapp.html");
+		rule.setTerminating(true); // this will stop Jetty from chaining the rewrites
+		rewriteHandler.addRule(rule);
+		
+
 		//Initialise paths
-		ResourceHandler resourceHandler = new ResourceHandler();
-		resourceHandler.setResourceBase(webappPath);
+		ResourceHandler staticHandler = new ResourceHandler();
+		staticHandler.setResourceBase(webappPath);
+		staticHandler.setDirAllowed(false);
+		//rewriteHandler.setHandler(staticHandler);
+		//staticHandler.setHandler(rewriteHandler);
+		//staticHandler.setWelcomeFiles(new String[]{"webapp.html"});
 		ResourceHandler reportsHandler = new ResourceHandler();
 		reportsHandler.setResourceBase(reportsPath);
-		//reportsHandler.setDirAllowed(false);
-		//resourceHandler.setPathInfoOnly(false);
-		ServletHandler dataHandler = new ServletHandler();
-		dataHandler.addServletWithMapping(WebServlet.class, "/data/*");
+		WebAppContext dynamicHandler = new WebAppContext();
+		dynamicHandler.setResourceBase("/");
+		dynamicHandler.addServlet(WebServlet.class, "/data/*");
+		//ServletHandler dataHandler = new ServletHandler();
+		//dataHandler.addServletWithMapping(WebServlet.class, "/data/*");
 
 		//Set Handlers
 		HandlerList handlers = new HandlerList();
-		handlers.setHandlers(new Handler[] { resourceHandler, reportsHandler, dataHandler});
+		handlers.setHandlers(new Handler[] {rewriteHandler, staticHandler, reportsHandler, dynamicHandler});
 		server.setHandler(handlers);
 
 		//Start other components
@@ -68,7 +88,7 @@ public class CoreServer {
 			CLI.error(Loc.CORE, "Server loop interrupted - "+e.toString());
 		}
 	}
-	
+
 	public static void shutdown() {
 		CLI.debug(Loc.CORE, "Starting shutdown...");
 		mqttManager.shutdownAll(); //Close MQTT clients
